6a8f189e7a80d91edc3db54dd2f9a95d6d28dfb4
Jed Smit <jedsmit1@gmail.com>
2019-10-28 16:13:29 -0700

    Added inital html and js files, initalized firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAzyiYXXVZbV6MEc_zkkfulbpjFj8_c-RE",
+    authDomain: "train-scheduler-2f68d.firebaseapp.com",
+    databaseURL: "https://train-scheduler-2f68d.firebaseio.com",
+    projectId: "train-scheduler-2f68d",
+    storageBucket: "train-scheduler-2f68d.appspot.com",
+    messagingSenderId: "1042424167571",
+    appId: "1:1042424167571:web:d2cf07a706df9db86e6166"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
